2f8cb2cfcb19f700262e0722089bfec915201f77
Nik <72612658+amp1re@users.noreply.github.com>
2023-11-29 14:14:51 +0300

    correct setup.py

--- setup.py
+++ setup.py
@@ -27,6 +27,8 @@ setup(
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10'
     ],
     description="ml_package_framework contains a set of classes and methods for simplified experimentation and packaging of models",
     entry_points={
