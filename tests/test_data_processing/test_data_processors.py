37f725bbe37fcd792401bd2f422cbee86a940c9d
Nik <72612658+amp1re@users.noreply.github.com>
2023-12-06 15:28:10 +0300

    fixed context manager

--- tests/test_data_processing/test_data_processors.py
+++ tests/test_data_processing/test_data_processors.py
@@ -1,4 +1,3 @@
-import os
 import tempfile
 
 import pandas as pd
@@ -13,9 +12,7 @@ def create_temp_csv(content: str) -> str:
     :param content: A string representing the content of the CSV file.
     :return: The path to the temporary CSV file.
     """
-    with tempfile.NamedTemporaryFile(
-        mode="w", delete=False, suffix=".csv"
-    ) as temp_file:
+    with tempfile.NamedTemporaryFile(mode="w", delete=True, suffix=".csv") as temp_file:
         with open(temp_file.name, "w", encoding="utf-8") as file:
             file.write(content)
             return temp_file.name
@@ -45,6 +42,3 @@ def test_load_data_with_temporary_file():
     assert list(data.columns) == ["col1", "col2"]
     assert data.iloc[0]["col1"] == "value1"
     assert data.iloc[1]["col2"] == "value4"
-
-    # Cleanup
-    os.remove(temp_csv_path)
