3a2cdd7d157eb6aab48c0c985a9cf08399848f70
Nik <72612658+amp1re@users.noreply.github.com>
2023-12-07 20:11:47 +0300

    update tests for data_processing

--- tests/test_data_processing/test_data_processors.py
+++ tests/test_data_processing/test_data_processors.py
@@ -1,44 +1,67 @@
+import os
 import tempfile
 
 import pandas as pd
+import pytest
 
 from ml_package_framework.data_processing import SberDataProcessor
 
 
-def create_temp_csv(content: str) -> str:
+@pytest.fixture(name="sample_data")
+def fixture_sample_data():
     """
-    Create a temporary CSV file with the given content.
+    Create a sample pandas DataFrame for use as a pytest fixture.
 
-    :param content: A string representing the content of the CSV file.
-    :return: The path to the temporary CSV file.
+    This fixture generates a DataFrame with predefined data. It's primarily intended
+    for use in testing contexts where a consistent and simple DataFrame is needed.
+
+    Returns
+    -------
+    pandas.DataFrame
+        A DataFrame with two columns ('col1' and 'col2') and two rows of sample data.
+        The values in 'col1' are ['value1', 'value2'], and the values in 'col2' are
+        ['value3', 'value4'].
     """
-    with tempfile.NamedTemporaryFile(mode="w", delete=True, suffix=".csv") as temp_file:
-        with open(temp_file.name, "w", encoding="utf-8") as file:
-            file.write(content)
-            return temp_file.name
+    data = {"col1": ["value1", "value2"], "col2": ["value3", "value4"]}
+
+    return pd.DataFrame(data)
 
 
-def test_load_data_with_temporary_file():
+def test_load_data_with_temporary_file(sample_data):
     """
-    Test the load_data method of SberDataProcessor.
+    Test the functionality of the SberDataProcessor class for loading data from a CSV file.
 
-    This test checks if the SberDataProcessor correctly loads data
-    from a CSV file into a pandas DataFrame.
-    It involves creating a temporary CSV file with predefined content, loading this file using the
-    SberDataProcessor, and then verifying the contents and structure of the resulting DataFrame.
-    Finally, it cleans up by removing the temporary file.
+    This test function validates whether the `load_data` method of the SberDataProcessor
+    class correctly reads data from a CSV file and whether the data read matches the
+    provided sample data. The test involves creating a temporary CSV file with the
+    sample data, using the SberDataProcessor to load this data, and then comparing the
+    loaded data against the original sample data.
+
+    Parameters
+    ----------
+    sample_data : pandas.DataFrame
+        A pandas DataFrame provided as a fixture. It contains the sample data for testing,
+        which is written to a CSV file and used for comparison with the loaded data.
+
+    Raises
+    ------
+    AssertionError
+        If the DataFrame loaded by the SberDataProcessor does not match the
+        sample_data DataFrame. This indicates a failure in the data loading process.
+
+    Notes
+    -----
+    The test passes if the `pd.testing.assert_frame_equal` function confirms that
+    the loaded DataFrame is identical to the sample DataFrame. This ensures the
+    integrity and correctness of the data loading process implemented in the
+    SberDataProcessor class.
     """
-    # Setup
-    processor = SberDataProcessor()
-    csv_content = "col1,col2\nvalue1,value2\nvalue3,value4"
-    temp_csv_path = create_temp_csv(csv_content)
 
-    # Execute
-    data = processor.load_data(temp_csv_path)
+    with tempfile.TemporaryDirectory() as temp_dir:
+        dst_path = os.path.join(temp_dir, "data.csv")
+        sample_data.to_csv(dst_path, index=False)
 
-    # Verify
-    assert isinstance(data, pd.DataFrame)
-    assert len(data) == 2  # Two rows of data
-    assert list(data.columns) == ["col1", "col2"]
-    assert data.iloc[0]["col1"] == "value1"
-    assert data.iloc[1]["col2"] == "value4"
+        processor = SberDataProcessor()
+        loaded_data = processor.load_data(dst_path)
+
+        pd.testing.assert_frame_equal(loaded_data, sample_data)
