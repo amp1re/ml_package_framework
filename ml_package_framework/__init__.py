a720604822a0ef3df27f6c1cd52806179750b880
Nik <72612658+amp1re@users.noreply.github.com>
2023-12-25 15:05:36 +0300

    update init

--- ml_package_framework/__init__.py
+++
@@ -1,9 +0,0 @@
-"""Top-level package for ml_package_framework."""
-
-__author__ = """Nikita Toloknov"""
-__email__ = "2360402@gmail.com"
-__version__ = "0.3.0"
-
-from .data_processing import DataProcessor, SberDataProcessor
-
-__all__ = ["DataProcessor", "SberDataProcessor"]
